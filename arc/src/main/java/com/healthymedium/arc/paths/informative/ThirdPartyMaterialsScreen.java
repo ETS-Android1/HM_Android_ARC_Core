3b27ceb7445bb5508e38779b9e07e28b376bb7b7
Michael Votaw <mike@thinkhappymedium.com>
2019-03-01 11:13:18 -0600

    (Issue NSARC-157) Added a "3rd Party Materials" view, and added button in About page to view it. Also setup the About page to pull text content from @string/about_app, so we don't need to setup an entirely separate view to display specialized content.

--- arc/src/main/java/com/healthymedium/arc/paths/informative/ThirdPartyMaterialsScreen.java
+++ arc/src/main/java/com/healthymedium/arc/paths/informative/ThirdPartyMaterialsScreen.java
@@ -5,25 +5,19 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewParent;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.healthymedium.arc.core.BaseFragment;
 import com.healthymedium.arc.font.Fonts;
 import com.healthymedium.arc.library.R;
 import com.healthymedium.arc.utilities.NavigationManager;
-import com.healthymedium.arc.utilities.ViewUtil;
 
-public class AboutScreen extends BaseFragment {
+
+public class ThirdPartyMaterialsScreen extends BaseFragment {
 
     TextView textViewBack;
-    TextView textViewHeader;
-    TextView textViewSubHeader;
-    Button button3rdParty;
 
-    public AboutScreen() {
+    public ThirdPartyMaterialsScreen() {
         allowBackPress(false);
         setEnterTransitionRes(R.anim.slide_in_right,R.anim.slide_in_left);
         setExitTransitionRes(R.anim.slide_out_left,R.anim.slide_out_right);
@@ -32,11 +26,7 @@ public class AboutScreen extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_about_app, container, false);
-
-        textViewHeader = view.findViewById(R.id.textViewHeader);
-        textViewSubHeader = view.findViewById(R.id.textViewSubHeader);
-        textViewSubHeader.setLineSpacing(ViewUtil.dpToPx(3),1.0f);
+        View view = inflater.inflate(R.layout.fragment_3rd_party_materials, container, false);
 
         textViewBack = view.findViewById(R.id.textViewBack);
         textViewBack.setTypeface(Fonts.robotoMedium);
@@ -49,15 +39,6 @@ public class AboutScreen extends BaseFragment {
 
         textViewBack.setVisibility(View.VISIBLE);
 
-        button3rdParty = view.findViewById(R.id.button3rdParty);
-
-        button3rdParty.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ThirdPartyMaterialsScreen thirdPartyScreen = new ThirdPartyMaterialsScreen();
-                NavigationManager.getInstance().open(thirdPartyScreen);
-            }
-        });
         return view;
     }
 
