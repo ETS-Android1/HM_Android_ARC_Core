a06d35ab754f8a0488daa93810b4e4b5e175a110
Spencer King <spencer@thinkhappymedium.com>
2019-03-11 13:32:47 -0500

    CRI-14, update grids test transition screens

--- arc/src/main/java/com/healthymedium/arc/paths/tests/GridLetters.java
+++ arc/src/main/java/com/healthymedium/arc/paths/tests/GridLetters.java
@@ -31,14 +31,14 @@ public class GridLetters extends BaseFragment {
 
     boolean paused;
     GridLayout gridLayout;
-    GridTestPathData gridTest;
-    GridTestPathData.Section section;
-    int eCount = 0;
-    int fCount = 0;
+    protected  GridTestPathData gridTest;
+    protected GridTestPathData.Section section;
+    protected int eCount = 0;
+    protected int fCount = 0;
 
-    TimedDialog dialog;
+    protected TimedDialog dialog;
     Handler handler;
-    Runnable runnable = new Runnable() {
+    protected Runnable runnable = new Runnable() {
         @Override
         public void run() {
             if(isVisible()){
