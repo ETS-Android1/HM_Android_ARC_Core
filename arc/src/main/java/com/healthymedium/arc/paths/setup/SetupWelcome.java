52156ca771a11ad0a54370b87f9cc4b422f69dc4
Spencer King <spencer@thinkhappymedium.com>
2019-05-01 14:42:01 -0500

    CRI-193, indicate app version

--- arc/src/main/java/com/healthymedium/arc/paths/setup/SetupWelcome.java
+++ arc/src/main/java/com/healthymedium/arc/paths/setup/SetupWelcome.java
@@ -18,6 +18,7 @@ import com.healthymedium.arc.paths.informative.ContactScreen;
 import com.healthymedium.arc.paths.informative.PrivacyScreen;
 import com.healthymedium.arc.study.Study;
 import com.healthymedium.arc.utilities.NavigationManager;
+import com.healthymedium.arc.utilities.VersionUtil;
 import com.healthymedium.arc.utilities.ViewUtil;
 
 public class SetupWelcome extends BaseFragment {
@@ -27,6 +28,7 @@ public class SetupWelcome extends BaseFragment {
     TextView textViewAboutApp;
     TextView textViewPrivacyPolicy;
     TextView textViewAppName;
+    TextView textViewVersion;
 
     public SetupWelcome() {
 
@@ -61,6 +63,10 @@ public class SetupWelcome extends BaseFragment {
                 Study.getPrivacyPolicy().show(getContext());
             }
         });
+
+        textViewVersion = view.findViewById(R.id.textViewVersion);
+        String ver = "v"+VersionUtil.getAppVersionName();
+        textViewVersion.setText(ver);
 
         button = view.findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
