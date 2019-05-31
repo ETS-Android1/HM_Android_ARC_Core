3a4d398f559a1e02c462cdf6938e4fffa0d33000
Steven Huff <stephen@thinkhappymedium.com>
2019-05-31 11:08:54 -0500

    add ability for custom buttons to show icons

--- arc/src/main/java/com/healthymedium/arc/custom/Button.java
+++ arc/src/main/java/com/healthymedium/arc/custom/Button.java
@@ -3,10 +3,12 @@ package com.healthymedium.arc.custom;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.healthymedium.arc.font.Fonts;
@@ -16,6 +18,7 @@ import com.healthymedium.arc.utilities.ViewUtil;
 public class Button extends FrameLayout {
 
     TextView textView;
+    ImageView imageView;
     View view;
     boolean inverted;
     Drawable shadow;
@@ -41,6 +44,7 @@ public class Button extends FrameLayout {
     private void init(Context context){
         view = inflate(context,R.layout.custom_button,this);
         textView = view.findViewById(R.id.textView);
+        imageView = view.findViewById(R.id.imageView);
         shadow = ViewUtil.getDrawable(R.drawable.btn_shadow);
         elevation = ViewUtil.dpToPx(3);
     }
@@ -49,13 +53,13 @@ public class Button extends FrameLayout {
     private void applyAttributeSet(Context context,AttributeSet attrs){
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.Button);
         try {
-
             textView.setText(a.getString(R.styleable.Button_text));
             inverted = a.getBoolean(R.styleable.Button_inverted,false);
             if(inverted){
                 textView.setTextColor(ViewUtil.getColor(R.color.primary));
                 textView.setBackgroundResource(R.drawable.button_inverted);
             }
+            setIcon(a.getDrawable(R.styleable.Button_icon));
             boolean enabled = a.getBoolean(R.styleable.Button_enabled,true);
             setEnabled(enabled);
 
@@ -80,7 +84,25 @@ public class Button extends FrameLayout {
         }
     }
 
+    public void clearText(){
+        textView.setText("");
+    }
+
     public void setText(String string){
+        imageView.setVisibility(GONE);
         textView.setText(string);
     }
+
+    public void setIcon(@DrawableRes int id) {
+        setIcon(ViewUtil.getDrawable(id));
+    }
+
+    public void setIcon(Drawable drawable){
+        if(drawable!=null) {
+            clearText();
+            imageView.setBackground(drawable);
+            imageView.setVisibility(VISIBLE);
+        }
+    }
+
 }
