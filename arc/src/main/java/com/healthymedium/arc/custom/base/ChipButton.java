f7b13a327ec1c81e5c18e967ea9d252ebe911fb7
Steven Huff <stephen@thinkhappymedium.com>
2019-07-23 16:55:51 -0500

    add base chip button

---
+++ arc/src/main/java/com/healthymedium/arc/custom/base/ChipButton.java
@@ -0,0 +1,188 @@
+package com.healthymedium.arc.custom.base;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.support.annotation.ColorInt;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.LinearLayout;
+
+import com.healthymedium.arc.library.R;
+import com.healthymedium.arc.utilities.ViewUtil;
+
+public class ChipButton extends LinearLayout {
+
+    private ValueAnimator enableAnimator;
+
+    LayerDrawable background;
+    ChipDrawable bottom;
+    ChipDrawable gradient;
+    FadingDrawable top;
+
+    float elevation;
+
+    public ChipButton(Context context) {
+        super(context);
+        init(null,0);
+    }
+
+    public ChipButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ChipButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+
+        Context context = getContext();
+
+        enableAnimator = new ValueAnimator();
+        enableAnimator.addUpdateListener(enableListener);
+
+        bottom = new ChipDrawable();
+        gradient = new ChipDrawable();
+        top = new FadingDrawable();
+
+        background = new LayerDrawable(new Drawable[]{bottom,gradient,top});
+        setBackground(background);
+
+        int color = ViewUtil.getColor(context,R.color.primary);
+
+        bottom.setFillColor(color);
+
+        int white = Color.argb(34,255,255,255);
+        int black = Color.argb(34,0,0,0);
+        gradient.setGradient(SimpleGradient.LINEAR_VERTICAL,white,black);
+
+        top.setStrokeWidth(ViewUtil.dpToPx(16));
+        top.setStrokeColor(color);
+        top.setFillColor(color);
+
+        elevation = getElevation();
+        setOnTouchListener(touchListener);
+    }
+
+    protected void setColor(@ColorInt int color) {
+        bottom.setFillColor(color);
+        top.setStrokeColor(color);
+        top.setFillColor(color);
+        invalidate();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        setOutlineProvider(bottom.getOutlineProvider());
+    }
+
+    @Override
+    public ViewOutlineProvider getOutlineProvider() {
+        return bottom.getOutlineProvider();
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        if(enabled) {
+            gradient.setAlpha(255);
+            top.setAlpha(255);
+            setElevation(elevation);
+            setAlpha(1.0f);
+        } else {
+            gradient.setAlpha(0);
+            top.setAlpha(0);
+            setElevation(0);
+            setAlpha(0.5f);
+        }
+    }
+
+    public void setEnabled(boolean enabled, boolean animate) {
+        if(!animate){
+            setEnabled(enabled);
+            return;
+        }
+        super.setEnabled(enabled);
+        int v0 = enabled ? 0:255;
+        int v1 = enabled ? 255:0;
+        enableAnimator.setIntValues(v0,v1);
+        enableAnimator.start();
+    }
+
+    ValueAnimator.AnimatorUpdateListener enableListener = new ValueAnimator.AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            int value = (int) animation.getAnimatedValue();
+            float percentage  = value/255f;
+            setAlpha((percentage/2f)+0.5f); // scale between 0.5 and 1.0
+            gradient.setAlpha(value);
+            top.setAlpha(value);
+            setElevation(percentage*elevation);
+            invalidate();
+        }
+    };
+
+    OnTouchListener touchListener = new OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = event.getAction();
+            switch (action) {
+                case MotionEvent.ACTION_UP:
+                    setElevation(elevation);
+                    gradient.setAlpha(255);
+                    top.setAlpha(255);
+                    invalidate();
+
+                    float x = event.getX();
+                    float y = event.getY();
+                    int w = v.getWidth();
+                    int h = v.getHeight();
+
+                    if(x>0 && x<w && y>0 && y<h){
+                        callOnClick();
+                    }
+                    break;
+                case MotionEvent.ACTION_DOWN:
+                    setElevation(0);
+                    gradient.setAlpha(0);
+                    top.setAlpha(0);
+                    invalidate();
+                    break;
+            }
+            return true;
+        }
+    };
+
+    public class FadingDrawable extends ChipDrawable {
+
+        float numberOfPasses = 20;
+
+        @Override
+        public void draw(Canvas canvas) {
+            if(drawFill){
+                canvas.drawPath(path,fillPaint);
+            }
+            if(drawStroke) {
+                float maxWidth = strokeWidth;
+
+                for (float i = 0; i <= numberOfPasses; i++){
+                    int alpha = (int) (i / numberOfPasses * 255f);
+                    float width = maxWidth * (1 - i / numberOfPasses);
+                    strokePaint.setAlpha(alpha);
+                    strokePaint.setStrokeWidth(width);
+                    canvas.drawPath(path, strokePaint);
+                }
+            }
+        }
+    }
+
+}
