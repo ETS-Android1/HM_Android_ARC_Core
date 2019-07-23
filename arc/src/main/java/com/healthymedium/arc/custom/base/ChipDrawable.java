9a8ceb00fadfd4089f6d374786a6ea9b108b647d
Steven Huff <stephen@thinkhappymedium.com>
2019-07-23 09:46:00 -0500

    add chip drawable

---
+++ arc/src/main/java/com/healthymedium/arc/custom/base/ChipDrawable.java
@@ -0,0 +1,50 @@
+package com.healthymedium.arc.custom.base;
+
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+
+public class ChipDrawable extends SimpleDrawable {
+
+    public ChipDrawable(){
+        super();
+    }
+
+    @Override
+    protected void updateOffsets() {
+
+        // create a rect that's small enough that the stroke isn't cut off
+        int offset = (int) (strokeWidth/2);
+        rect.set(offset,offset,width-offset,height-offset);
+
+    }
+
+    protected Path getPath(Rect rect) {
+
+        Path path = new Path();
+
+        int radius = height;
+
+        path.moveTo(rect.left + radius, rect.top);
+
+        // top line
+        path.lineTo(rect.right - radius, rect.top);
+
+        // right arc
+        RectF rightRect = new RectF(rect.right - radius, rect.top, rect.right, rect.bottom);
+        path.arcTo(rightRect, 270F, 180F, false);
+
+        // bottom line
+        path.lineTo(rect.left + radius, rect.bottom);
+
+        // left arc
+        RectF leftRect = new RectF(rect.left, rect.top, rect.left + radius, rect.bottom);
+        path.arcTo(leftRect, 90F, 180F, false);
+
+        path.close();
+
+        return path;
+    }
+
+
+}
