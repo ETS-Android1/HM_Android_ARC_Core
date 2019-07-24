4a07cda97f83017029e869788f0c80024a020ae9
Steven Huff <stephen@thinkhappymedium.com>
2019-07-24 15:49:54 -0500

    remove unused fonts

--- arc/src/main/java/com/healthymedium/arc/font/Fonts.java
+++ arc/src/main/java/com/healthymedium/arc/font/Fonts.java
@@ -6,26 +6,11 @@ public class Fonts {
 
     private static boolean loaded = false;
     public static Typeface georgia;
-    public static Typeface georgiaBold;
-    public static Typeface georgiaBoldItalic;
     public static Typeface georgiaItalic;
 
     public static Typeface roboto;
-    public static Typeface robotoBold;
-    public static Typeface robotoBoldItalic;
-    public static Typeface robotoItalic;
-
-    public static Typeface robotoLight;
-    public static Typeface robotoLightItalic;
-
     public static Typeface robotoMedium;
-    public static Typeface robotoMediumItalic;
-
-    public static Typeface robotoThin;
-    public static Typeface robotoThinItalic;
-
-    public static Typeface tahoma;
-    public static Typeface tahomaBold;
+    public static Typeface robotoBold;
 
     public static void load(){
         if(loaded){
@@ -34,26 +19,12 @@ public class Fonts {
         FontFactory fontFactory = FontFactory.getInstance();
         if(fontFactory != null) {
             georgia = fontFactory.getFont("fonts/Georgia.ttf");
-            georgiaBold = fontFactory.getFont("fonts/Georgia-Bold.ttf");
-            georgiaBoldItalic = fontFactory.getFont("fonts/Georgia-BoldItalic.ttf");
             georgiaItalic = fontFactory.getFont("fonts/Georgia-Italic.ttf");
 
             roboto = fontFactory.getFont("fonts/Roboto-Regular.ttf");
-            robotoBold = fontFactory.getFont("fonts/Roboto-Bold.ttf");
-            robotoBoldItalic = fontFactory.getFont("fonts/Roboto-BoldItalic.ttf");
-            robotoItalic = fontFactory.getFont("fonts/Roboto-Italic.ttf");
-
-            robotoLight = fontFactory.getFont("fonts/Roboto-Light.ttf");
-            robotoLightItalic = fontFactory.getFont("fonts/Roboto-LightItalic.ttf");
-
             robotoMedium = fontFactory.getFont("fonts/Roboto-Medium.ttf");
-            robotoMediumItalic = fontFactory.getFont("fonts/Roboto-MediumItalic.ttf");
+            robotoBold = fontFactory.getFont("fonts/Roboto-Bold.ttf");
 
-            robotoThin = fontFactory.getFont("fonts/Roboto-Thin.ttf");
-            robotoThinItalic = fontFactory.getFont("fonts/Roboto-ThinItalic.ttf");
-
-            tahoma = fontFactory.getFont("fonts/Tahoma.ttf");
-            tahomaBold = fontFactory.getFont("fonts/Tahoma-Bold.ttf");
             loaded = true;
         }
     }
