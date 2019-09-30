087fc148aa6bcd29e602f09c5175e50da885a904
Steven Huff <steven@thinkhappymedium.com>
2019-09-30 10:36:33 -0500

    add mock analytics module

---
+++ mock_analytics/src/main/java/com/crashlytics/android/Crashlytics.java
@@ -0,0 +1,17 @@
+package com.crashlytics.android;
+
+public class Crashlytics {
+
+    public static void logException(Throwable throwable) {
+
+    }
+
+    public static void log(String msg) {
+
+    }
+
+    public static void log(int priority, String tag, String msg) {
+
+    }
+
+}
---
+++ mock_analytics/src/main/java/io/fabric/sdk/android/Fabric.java
@@ -0,0 +1,12 @@
+package io.fabric.sdk.android;
+
+import android.content.Context;
+import com.crashlytics.android.Crashlytics;
+
+public class Fabric {
+	
+	public static Fabric with(Context context, Crashlytics crashlytics) {
+        return null;
+    }
+
+}
