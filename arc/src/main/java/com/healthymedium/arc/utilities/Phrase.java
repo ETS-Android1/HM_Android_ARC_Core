d9041493899557a3d5f0c7b243690f8721634354
Steven Huff <steven@thinkhappymedium.com>
2019-10-18 14:02:33 -0500

    phrase - update a couple of token targets

--- arc/src/main/java/com/healthymedium/arc/utilities/Phrase.java
+++ arc/src/main/java/com/healthymedium/arc/utilities/Phrase.java
@@ -27,6 +27,16 @@ public class Phrase {
         phrase = getString(id);
     }
 
+    public Phrase(Context context, String string){
+        this.context = context;
+        phrase = string;
+    }
+
+    public Phrase(String string){
+        this.context = Application.getInstance();
+        phrase = string;
+    }
+
     private String getString(@StringRes int id){
         return context.getString(id);
     }
@@ -36,75 +46,91 @@ public class Phrase {
     public void replace(@StringRes int tokenId, @StringRes int stringId){
         String token = getString(tokenId);
         String string = getString(stringId);
-        replace(token,string);
+        replace(token, string);
     }
 
     public void replace(@StringRes int tokenId, String string){
         String token = getString(tokenId);
-        replace(token,string);
+        replace(token, string);
     }
 
     public void replace(String token, String string){
-        phrase = phrase.replace(token,string);
+        phrase = phrase.replace(token, string);
     }
 
     // ---------------------------------------------------------------------------------------------
 
     public void replaceDate(String date){
-        replace(R.string.token_date,date);
+        replace(R.string.token_date, date);
     }
 
     public void replaceDates(String date1, String date2){
-        replace(R.string.token_date1,date1);
-        replace(R.string.token_date2,date2);
+        replace(R.string.token_date1, date1);
+        replace(R.string.token_date2, date2);
     }
 
     public void replaceDate(@StringRes int format, DateTime dateTime){
-        String string = dateTime.toString(getString(format),Application.getInstance().getLocale());
+        String string = dateTime.toString(getString(format), Application.getInstance().getLocale());
         replaceDate(string);
     }
 
+    public void replaceDate(@StringRes int tokenId, @StringRes int format, DateTime dateTime){
+        String string = dateTime.toString(getString(format), Application.getInstance().getLocale());
+        replace(tokenId,string);
+    }
+
     public void replaceDates(@StringRes int format, DateTime date1, DateTime date2){
-        replaceDate(format,date1);
-        replaceDate(format,date2);
+        replaceDate(R.string.token_date1, format, date1);
+        replaceDate(R.string.token_date2, format, date2);
     }
 
     // ---------------------------------------------------------------------------------------------
+    // time
 
     public void replaceTime(@StringRes int format, DateTime time){
-        String string = time.toString(getString(format),Application.getInstance().getLocale());
+        String string = time.toString(getString(format), Application.getInstance().getLocale());
         replaceTime(string);
-    }
-
-    public void replaceTimes(@StringRes int format, DateTime time1, DateTime time2){
-        replaceTime(format,time1);
-        replaceTime(format,time2);
     }
 
     public void replaceTime(@StringRes int format, LocalTime time){
-        String string = time.toString(getString(format),Application.getInstance().getLocale());
+        String string = time.toString(getString(format), Application.getInstance().getLocale());
         replaceTime(string);
     }
 
-    public void replaceTimes(@StringRes int format, LocalTime time1, LocalTime time2){
-        replaceTime(format,time1);
-        replaceTime(format,time2);
+    public void replaceTime(@StringRes int tokenId, @StringRes int format, LocalTime time){
+        String string = time.toString(getString(format), Application.getInstance().getLocale());
+        replace(tokenId, string);
     }
 
     public void replaceTime(String time){
-        replace(R.string.token_time,time);
+        replace(R.string.token_time, time);
+    }
+
+    public void replaceTime(@StringRes int tokenId, @StringRes int format, DateTime time){
+        String string = time.toString(getString(format), Application.getInstance().getLocale());
+        replace(tokenId, string);
+    }
+
+    public void replaceTimes(@StringRes int format, DateTime time1, DateTime time2){
+        replaceTime(R.string.token_time1, format, time1);
+        replaceTime(R.string.token_time2, format, time2);
+    }
+
+    public void replaceTimes(@StringRes int format, LocalTime time1, LocalTime time2){
+        replaceTime(R.string.token_time1, format, time1);
+        replaceTime(R.string.token_time2, format, time2);
     }
 
     public void replaceTimes(String time1, String time2){
-        replace(R.string.token_time1,time1);
-        replace(R.string.token_time2,time2);
+        replace(R.string.token_time1, time1);
+        replace(R.string.token_time2, time2);
     }
 
     // ---------------------------------------------------------------------------------------------
 
     public void replaceValues(String value1, String value2){
-        replace(R.string.token_value1,value1);
-        replace(R.string.token_value1,value2);
+        replace(R.string.token_value1, value1);
+        replace(R.string.token_value1, value2);
     }
 
     public void replaceAmount(String amount) {
