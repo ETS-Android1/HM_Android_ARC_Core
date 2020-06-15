0bca8d2d9fd62b2a91d5bb1314b0310c95e3a971
Steven Huff <stephen@thinkhappymedium.com>
2020-06-15 14:54:43 -0500

    ARC-8; Study Summary - add api model

---
+++ arc/src/main/java/com/healthymedium/arc/api/models/StudySummary.java
@@ -0,0 +1,10 @@
+package com.healthymedium.arc.api.models;
+
+public class StudySummary {
+
+    public String total_earnings;
+    public Integer tests_taken;
+    public Integer days_tested;
+    public Integer goals_met;
+
+}
