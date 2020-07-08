cc364d6761431d9f887d044af3564caeb2b02b8a
Michelle <michelle@thinkhappymedium.com>
2020-07-08 16:41:23 -0500

    grid2 choice dialog - inflate view

--- arc/src/main/java/com/healthymedium/arc/ui/Grid2ChoiceDialog.java
+++ arc/src/main/java/com/healthymedium/arc/ui/Grid2ChoiceDialog.java
@@ -3,6 +3,7 @@ package com.healthymedium.arc.ui;
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.healthymedium.arc.library.R;
 import com.healthymedium.arc.ui.base.PointerDialog;
@@ -10,6 +11,10 @@ import com.healthymedium.arc.utilities.ViewUtil;
 
 public class Grid2ChoiceDialog extends PointerDialog {
 
+    Grid2ChoiceView phone;
+    Grid2ChoiceView key;
+    Grid2ChoiceView pen;
+    TextView textViewGridDialog;
 
     public Grid2ChoiceDialog(Activity activity, View target, int pointerConfig) {
         super(activity, target, null, pointerConfig);
@@ -22,15 +27,22 @@ public class Grid2ChoiceDialog extends PointerDialog {
     }
 
     private void init() {
-        // todo: actually inflate view
-        // View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_grid2_choice,null);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_grid2_choice,null);
 
+        phone = view.findViewById(R.id.phone);
+        key = view.findViewById(R.id.key);
+        pen = view.findViewById(R.id.pen);
 
+        phone.setImage(R.drawable.phone);
+        key.setImage(R.drawable.key);
+        pen.setImage(R.drawable.pen);
 
+        textViewGridDialog = view.findViewById(R.id.textViewGridDialog);
+        textViewGridDialog.setText("Select the item that was here");
 
         setElevation(ViewUtil.dpToPx(4));
         setRadius(16);
-        //setView(view);
+        setView(view);
     }
 
 }
