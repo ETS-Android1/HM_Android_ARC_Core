a83a5346208785e6fd9ca529b7a737e251399f7e
Steven Huff <stephen@thinkhappymedium.com>
2020-07-10 13:37:27 -0500

    init grid2 screens

---
+++ arc/src/main/java/com/healthymedium/arc/paths/tests/Grid2Study.java
@@ -0,0 +1,46 @@
+package com.healthymedium.arc.paths.tests;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.healthymedium.arc.core.BaseFragment;
+import com.healthymedium.arc.library.R;
+
+public class Grid2Study extends BaseFragment {
+
+    public Grid2Study() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_grid2_study, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+}
---
+++ arc/src/main/java/com/healthymedium/arc/paths/tests/Grid2Test.java
@@ -0,0 +1,46 @@
+package com.healthymedium.arc.paths.tests;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.healthymedium.arc.core.BaseFragment;
+import com.healthymedium.arc.library.R;
+
+public class Grid2Test extends BaseFragment {
+
+    public Grid2Test() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_grid2_test, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+}
