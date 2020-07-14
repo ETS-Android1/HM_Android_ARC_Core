c8123bbdc32d94295c2517444ea75bea5f6cdb9f
Michelle <michelle@thinkhappymedium.com>
2020-07-14 10:34:41 -0500

    grid2 test - add basics to begin working on letters section

--- arc/src/main/java/com/healthymedium/arc/paths/tests/Grid2Letters.java
+++ arc/src/main/java/com/healthymedium/arc/paths/tests/Grid2Letters.java
@@ -1,15 +1,30 @@
 package com.healthymedium.arc.paths.tests;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 import com.healthymedium.arc.core.BaseFragment;
+import com.healthymedium.arc.font.Fonts;
 import com.healthymedium.arc.library.R;
+import com.healthymedium.arc.path_data.GridTestPathData;
+import com.healthymedium.arc.utilities.ViewUtil;
 
 public class Grid2Letters extends BaseFragment {
+
+    boolean paused;
+    GridLayout gridLayout;
+    protected GridTestPathData gridTest;
+    protected GridTestPathData.Section section;
+    protected int eCount = 0;
+    protected int fCount = 0;
+
+    private TextView textViewTapFsLabel;
 
     public Grid2Letters() {
 
@@ -24,6 +39,19 @@ public class Grid2Letters extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_grid2_letters, container, false);
+        gridLayout = view.findViewById(R.id.gridLayout);
+
+        textViewTapFsLabel = view.findViewById(R.id.tapGridText);
+        textViewTapFsLabel.setText(ViewUtil.getHtmlString(R.string.grids_subheader_fs));
+
+        //TODO: listener
+
+        Typeface font = Fonts.georgia;
+        int size = gridLayout.getChildCount();
+        for(int i=0;i<size;i++){
+            ((TextView)gridLayout.getChildAt(i)).setTypeface(font);
+        }
+
         return view;
     }
 
