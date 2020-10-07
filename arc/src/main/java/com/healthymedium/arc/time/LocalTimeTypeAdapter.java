e5b345c8bb651eccba1d0238b89e03baf517e84c
Steven Huff <stephen@thinkhappymedium.com>
2020-10-07 15:10:44 -0500

    EXR-1052, local time type adapter - force US English and handle invalid formats

--- arc/src/main/java/com/healthymedium/arc/time/LocalTimeTypeAdapter.java
+++ arc/src/main/java/com/healthymedium/arc/time/LocalTimeTypeAdapter.java
@@ -7,22 +7,47 @@ import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
+import com.healthymedium.analytics.Log;
 
 import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+
 import java.lang.reflect.Type;
+import java.util.Locale;
 
 public class LocalTimeTypeAdapter implements JsonSerializer<LocalTime>, JsonDeserializer<LocalTime> {
 
-    @Override
-    public JsonElement serialize(LocalTime src, Type srcType, JsonSerializationContext context) {
-        return new JsonPrimitive(src.toString());
+    DateTimeFormatter formatter;
+
+    public LocalTimeTypeAdapter(){
+        formatter = new DateTimeFormatterBuilder()
+                .appendPattern("h:mm a")
+                .toFormatter()
+                .withLocale(Locale.US);
     }
 
     @Override
-    public LocalTime deserialize(JsonElement json, Type type, JsonDeserializationContext context)
-            throws JsonParseException {
-       // long millis = json.getAsJsonObject().get("iLocalMillis").getAsLong();
-        return new LocalTime(json.getAsString());
+    public JsonElement serialize(LocalTime src, Type srcType, JsonSerializationContext context) {
+        return new JsonPrimitive(src.toString(formatter));
+    }
+
+    @Override
+    public LocalTime deserialize(JsonElement json, Type type, JsonDeserializationContext context) throws JsonParseException {
+        String string = json.getAsString();
+        LocalTime time = null;
+        try {
+            time = LocalTime.parse(string,formatter);
+        } catch (IllegalArgumentException e) {
+            Log.system.w("LocalTimeTypeAdapter","failed to deserialize '"+string+"', attempting fallback");
+            string = string
+                    .replace("a.m.","AM")
+                    .replace("p.m.","PM")
+                    .replace("午前","AM")
+                    .replace("午後","PM");
+            time = LocalTime.parse(string,formatter);
+        }
+        return time;
     }
 
 }
