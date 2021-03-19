43f9c6fd778ccfc2eb833daa28e23b10eeb75004
Mike DePhillips <mike@sdpdigital.com>
2021-03-19 09:49:49 -0700

    Removed initialization threading on splash screen

--- arc/src/main/java/com/healthymedium/arc/core/SplashScreen.java
+++ arc/src/main/java/com/healthymedium/arc/core/SplashScreen.java
@@ -2,7 +2,6 @@ package com.healthymedium.arc.core;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -40,9 +39,63 @@ public class SplashScreen extends BaseFragment {
         if(!visible) {
             view.setVisibility(View.INVISIBLE);
         }
-        AsyncLoader loader = new AsyncLoader();
-        loader.execute();
+
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstance) {
+        super.onViewCreated(view, savedInstance);
+        initializeApp();
+    }
+
+    private void initializeApp() {
+        Context context = getContext();
+        getMainActivity().setupKeyboardWatcher();
+
+        Application.getInstance().updateLocale(getContext());
+
+        if(FontFactory.getInstance()==null) {
+            FontFactory.initialize(context);
+        }
+
+        if(!Fonts.areLoaded()){
+            Fonts.load();
+            FontFactory.getInstance().setDefaultFont(Fonts.roboto);
+            FontFactory.getInstance().setDefaultBoldFont(Fonts.robotoBold);
+        }
+
+        Hints.load();
+
+        TestCycle cycle = Study.getCurrentTestCycle();
+        if(cycle != null){
+            if(cycle.getActualStartDate().isBeforeNow() && cycle.getActualEndDate().isAfterNow()){
+                Proctor.startService(getContext());
+            }
+        }
+
+        // We need to check to see if we're currently in the middle of a test session.
+        // If we are, and if the state machine has valid fragments, we should let it continue
+        // displaying those.
+        // Otherwise, just run the Study instance, and let it figure out where it needs to be.
+
+        if(Study.getParticipant().isCurrentlyInTestSession()
+                && Study.getParticipant().checkForTestAbandonment() == false
+                && Study.getStateMachine().hasValidFragments()
+        ) {
+            skipSegment = true;
+        } else {
+            skipSegment = false;
+            Study.getInstance().run();
+        }
+
+        ready = true;
+
+        getMainActivity().getWindow().setBackgroundDrawableResource(R.drawable.core_background);
+
+        if(!paused){
+            exit();
+        }
     }
 
     @Override
@@ -62,73 +115,6 @@ public class SplashScreen extends BaseFragment {
         paused = true;
     }
 
-
-    private class AsyncLoader extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            Context context = getContext();
-            getMainActivity().setupKeyboardWatcher();
-
-            Application.getInstance().updateLocale(getContext());
-
-            if(FontFactory.getInstance()==null) {
-                FontFactory.initialize(context);
-            }
-
-            if(!Fonts.areLoaded()){
-                Fonts.load();
-                FontFactory.getInstance().setDefaultFont(Fonts.roboto);
-                FontFactory.getInstance().setDefaultBoldFont(Fonts.robotoBold);
-            }
-
-            Hints.load();
-
-            TestCycle cycle = Study.getCurrentTestCycle();
-            if(cycle !=null){
-                if(cycle.getActualStartDate().isBeforeNow() && cycle.getActualEndDate().isAfterNow()){
-                    Proctor.startService(getContext());
-                }
-            }
-
-            // We need to check to see if we're currently in the middle of a test session.
-            // If we are, and if the state machine has valid fragments, we should let it continue
-            // displaying those.
-            // Otherwise, just run the Study instance, and let it figure out where it needs to be.
-
-            if(Study.getParticipant().isCurrentlyInTestSession()
-                && Study.getParticipant().checkForTestAbandonment() == false
-                && Study.getStateMachine().hasValidFragments()
-            ) {
-                skipSegment = true;
-            } else {
-                skipSegment = false;
-                Study.getInstance().run();
-            }
-
-            ready = true;
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void etc) {
-            if(getMainActivity()==null) {
-                return;
-            }
-
-            getMainActivity().getWindow().setBackgroundDrawableResource(R.drawable.core_background);
-
-            if(!paused){
-                exit();
-            }
-        }
-    }
-
     private void exit(){
         if(getFragmentManager() != null) {
             getFragmentManager().popBackStack();
@@ -139,5 +125,4 @@ public class SplashScreen extends BaseFragment {
             }
         }
     }
-
 }
